ecb0b82977f1e1a2641024dafe32f34d300f1e4c
KaanBoncukcu <kaan.boncukcu@shipsgo.com>
2023-04-14 11:11:08 +0300

    bug fix try

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -55,7 +55,7 @@ class PostController extends Controller
             $createPostIngredients[] = $ingredient;
         }
 
-
+        dd($createPostTags);
         PostTag::insert($createPostTags);
 
         PostStep::insert($createPostSteps);
